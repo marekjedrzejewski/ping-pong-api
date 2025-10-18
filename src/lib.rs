25b88d37e480f46e255ffb1a230b79454251215f
Marek Jędrzejewski <kontakt@marekjedrzejewski.pl>
2025-10-18 22:13:50 +0200

    Add GameState and expose it on "/"
    Still only showing initial state as it's not modified

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-use axum::{Router, extract::State, routing::get};
+use axum::{Json, Router, extract::State, http::StatusCode, routing::get};
+use serde::Serialize;
 
-#[derive(Clone)]
+#[derive(Clone, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum Side {
     Ping,
     Pong,
@@ -23,28 +25,51 @@ impl Side {
 impl fmt::Display for Side {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            Side::Ping => write!(f, "Ping"),
-            Side::Pong => write!(f, "Pong"),
+            Side::Ping => write!(f, "ping"),
+            Side::Pong => write!(f, "pong"),
         }
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Serialize)]
+pub struct Score {
+    pub ping: usize,
+    pub pong: usize,
+}
+
+#[derive(Clone, Serialize)]
+pub struct GameState {
+    pub server: Side,
+    pub score: Score,
+}
+
+#[derive(Clone, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct AppState {
     pub current_side: Arc<RwLock<Side>>,
+    pub overall_game_state: Arc<RwLock<GameState>>,
+}
+
+pub fn create_initial_state() -> AppState {
+    AppState {
+        current_side: Arc::new(RwLock::new(Side::Ping)),
+        overall_game_state: Arc::new(RwLock::new(GameState {
+            server: Side::Ping,
+            score: Score { ping: 0, pong: 0 },
+        })),
+    }
 }
 
 pub fn create_app(state: AppState) -> Router {
     Router::new()
-        .route("/", get(spectate))
+        .route("/", get(get_state))
         .route("/ping", get(ping))
         .route("/pong", get(pong))
         .with_state(state)
 }
 
-async fn spectate(State(state): State<AppState>) -> String {
-    let data = state.current_side.read().expect("read lock was poisoned");
-    (*data).to_string()
+async fn get_state(State(state): State<AppState>) -> (StatusCode, Json<AppState>) {
+    (StatusCode::OK, Json(state))
 }
 async fn ping(State(state): State<AppState>) -> String {
     let mut data = state.current_side.write().expect("write lock was poisoned");
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,13 +1,10 @@
-use std::sync::{Arc, RwLock};
-
 use axum_test::TestServer;
-use ping_pong_api::{AppState, Side, create_app};
+use ping_pong_api::{create_app, create_initial_state};
+use serde_json::json;
 
 #[tokio::test]
 async fn play_some_ping_pong() {
-    let state = AppState {
-        current_side: Arc::new(RwLock::new(Side::Ping)),
-    };
+    let state = create_initial_state();
     let app = create_app(state);
     let server = TestServer::builder()
         .mock_transport()
@@ -16,26 +13,32 @@ async fn play_some_ping_pong() {
 
     let root_response = server.get("/").await;
     root_response.assert_status_ok();
-    root_response.assert_text("Ping");
+    root_response.assert_json_contains(&json!({
+        "currentSide": "ping"
+    }));
 
     // can't pong if it's ping turn
     let pong_response = server.get("/pong").await;
     pong_response.assert_text("MISS");
 
     let ping_response = server.get("/ping").await;
-    ping_response.assert_text("Pong");
+    ping_response.assert_text("pong");
 
     // NOT SO FAST, WAIT FOR YOUR TURN!
     let ping_response = server.get("/ping").await;
     ping_response.assert_text("MISS");
 
     let pong_response = server.get("/pong").await;
-    pong_response.assert_text("Ping");
+    pong_response.assert_text("ping");
 
     for _n in 0..50 {
-        server.get("/ping").await.assert_text("Pong");
-        server.get("/").await.assert_text("Pong");
-        server.get("/pong").await.assert_text("Ping");
-        server.get("/").await.assert_text("Ping");
+        server.get("/ping").await.assert_text("pong");
+        server.get("/").await.assert_json_contains(&json!({
+            "currentSide": "pong"
+        }));
+        server.get("/pong").await.assert_text("ping");
+        server.get("/").await.assert_json_contains(&json!({
+            "currentSide": "ping"
+        }));
     }
 }
