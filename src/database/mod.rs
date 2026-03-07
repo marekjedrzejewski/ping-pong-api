ce20f6d077eed05958220281204768474c808093
Marek Jędrzejewski <kontakt@marekjedrzejewski.pl>
2026-03-08 00:05:15 +0100

    Move DbError to separate file

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,14 +1,12 @@
+mod db_error;
 use crate::models::{
     application::GameTables,
     game::{GameState, TableState},
 };
+pub use db_error::DbError;
 use log::info;
-use sqlx::{PgPool, migrate::MigrateError, postgres::PgPoolOptions};
-use std::{
-    env::{self, VarError},
-    error::Error,
-    fmt,
-};
+use sqlx::{PgPool, postgres::PgPoolOptions};
+use std::env;
 
 /// TableUid has constraints on its format enforced by the database.
 /// Specifying different type to be clear that this is not just any string
@@ -103,61 +101,4 @@ pub async fn update_game_state(
     tx.commit().await?;
 
     Ok(())
-}
-
-#[derive(Debug)]
-pub enum DbError {
-    EnvVar(VarError),
-    Connection(sqlx::Error),
-    Migration(MigrateError),
-    Decoding(serde_json::Error),
-    RowNotFound,
-}
-
-impl From<VarError> for DbError {
-    fn from(value: VarError) -> Self {
-        DbError::EnvVar(value)
-    }
-}
-
-impl From<sqlx::Error> for DbError {
-    fn from(value: sqlx::Error) -> Self {
-        DbError::Connection(value)
-    }
-}
-
-impl From<MigrateError> for DbError {
-    fn from(value: MigrateError) -> Self {
-        DbError::Migration(value)
-    }
-}
-
-impl From<serde_json::Error> for DbError {
-    fn from(value: serde_json::Error) -> Self {
-        DbError::Decoding(value)
-    }
-}
-
-impl fmt::Display for DbError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            DbError::EnvVar(e) => write!(f, "Database environment variable error: {}", e),
-            DbError::Connection(e) => write!(f, "Database connection error: {}", e),
-            DbError::Migration(e) => write!(f, "Database migration failed: {}", e),
-            DbError::Decoding(e) => write!(f, "Value decoding failed: {}", e),
-            DbError::RowNotFound => write!(f, "Row not found"),
-        }
-    }
-}
-
-impl Error for DbError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            DbError::EnvVar(e) => Some(e),
-            DbError::Connection(e) => Some(e),
-            DbError::Migration(e) => Some(e),
-            DbError::Decoding(e) => Some(e),
-            DbError::RowNotFound => None,
-        }
-    }
 }
